ea2d662df21283b27258c21ed572a30b1abfad26
monty-georgiev <monty.georgiev@gmail.com>
2016-12-26 19:56:55 +0200

    Added Caesar cipher to the repository

---
+++ 1.Ceasar.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+void caesar_cipher(string inputText, bool encode)
+{
+
+    string outputString = "";
+    char outputChar;
+
+    for (int i = 0; i < inputText.length(); i++)
+    {
+        if (encode)
+        {
+            outputChar = (char)((((tolower(inputText[i]) + 3) - 97) % 26) + 97);
+        }
+        else
+        {
+            outputChar = (char)((((tolower(inputText[i]) - 3) - 97) % 26) + 97);
+        }
+
+        outputString += outputChar;
+    }
+
+    cout << outputString + "\n";
+}
+
+int main()
+{
+    string choice = "";
+    string text;
+
+    while (true)
+    {
+        cout << "Would you like to (e)ncode or (d)ecode? ";
+        cin >> choice;
+
+        if (choice == "e")
+        {
+            cout << "Enter the text to encode: \n";
+            cin >> text;
+            caesar_cipher(text, true);
+            break;
+        }
+
+        if (choice == "d")
+        {
+            cout << "Enter the text to decode: \n";
+            cin >> text;
+            caesar_cipher(text, false);
+            break;
+        }
+    }
+
+    return 0;
+}
